94e9c38b25e76d6d3a6e0c4f5f24525adc614527
Stuart Kennedy <rav2040@gmail.com>
2023-04-12 14:44:28 +1200

    Implement write stream

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,18 @@
-import { mkdir, writeFile } from "fs/promises";
+import { mkdir } from "fs/promises";
+import { createWriteStream } from "fs";
 import { join, dirname } from "path";
-import { getInput, setFailed } from "@actions/core";
+import { getInput, getMultilineInput, setFailed } from "@actions/core";
 import { S3Client, ListObjectsV2Command, GetObjectCommand } from "@aws-sdk/client-s3";
-
-const PATH_SPLIT_REGEX = /\s+(?=([^"]*"[^"]*")*[^"]*$)/g;
 
 async function main() {
     try {
-        const bucket = getInput("bucket");
-        const path = getInput("path");
+        const bucket = getInput("bucket", { required: true });
+        const path = getMultilineInput("path", { required: true });
         const prefix = getInput("prefix");
-
-        const paths = path.split(PATH_SPLIT_REGEX).filter(Boolean);
-        const uniquePaths = Array.from(new Set(paths));
 
         const s3 = new S3Client({});
 
-        const keys = (await Promise.all(uniquePaths.map(async (path) => {
+        const keys = (await Promise.all(path.map(async (path) => {
             const listObjectsCommand = new ListObjectsV2Command({
                 Bucket: bucket,
                 Prefix: [prefix, path].filter(Boolean).join("/"),
@@ -29,20 +25,36 @@ async function main() {
         const uniqueKeys = Array.from(new Set(keys));
 
         await Promise.all(uniqueKeys.map(async (key) => {
+            const filename = join(process.cwd(), prefix.length ? key.slice(key.indexOf("/") + 1) : key);
+
             const getObjectCommand = new GetObjectCommand({
                 Bucket: bucket,
                 Key: key,
             });
-
             const response = await s3.send(getObjectCommand);
 
+            await mkdir(dirname(filename), { recursive: true });
+
             if (response.Body !== undefined) {
-                const filename = join(process.cwd(), prefix.length ? key.slice(key.indexOf("/") + 1) : key);
-                const data = await response.Body.transformToByteArray();
-                await mkdir(dirname(filename), { recursive: true });
-                await writeFile(filename, data);
-                console.info("Downloaded:", filename);
+                const reader = response.Body.transformToWebStream().getReader();
+                const fileStream = createWriteStream(filename);
+
+                const writeToFile: () => Promise<void> = async () => {
+                    const { value, done } = await reader.read();
+
+                    if (value !== undefined) {
+                        fileStream.write(value);
+                    } else {
+                        fileStream.end();
+                    }
+
+                    if (!done) return writeToFile();
+                };
+
+                await writeToFile();
             }
+
+            console.info("Downloaded:", filename);
         }))
     } catch (err) {
         if (err instanceof Error) setFailed(err);
