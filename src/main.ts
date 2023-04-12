8add961bbfd701d4487ffe1fca74ef93e055d052
Stuart Kennedy <rav2040@gmail.com>
2023-04-12 15:31:46 +1200

    Use file stream to write

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,20 @@
-import { mkdir, writeFile } from "fs/promises";
-import { join, dirname } from "path";
-import { getInput, setFailed } from "@actions/core";
-import { S3Client, ListObjectsV2Command, GetObjectCommand } from "@aws-sdk/client-s3";
+import type { Readable } from "stream";
 
-const PATH_SPLIT_REGEX = /\s+(?=([^"]*"[^"]*")*[^"]*$)/g;
+import { mkdir } from "fs/promises";
+import { createWriteStream } from "fs";
+import { join, dirname } from "path";
+import { getInput, getMultilineInput, setFailed } from "@actions/core";
+import { S3Client, ListObjectsV2Command, GetObjectCommand } from "@aws-sdk/client-s3";
 
 async function main() {
     try {
-        const bucket = getInput("bucket");
-        const path = getInput("path");
+        const bucket = getInput("bucket", { required: true });
+        const path = getMultilineInput("path", { required: true });
         const prefix = getInput("prefix");
-
-        const paths = path.split(PATH_SPLIT_REGEX).filter(Boolean);
-        const uniquePaths = Array.from(new Set(paths));
 
         const s3 = new S3Client({});
 
-        const keys = (await Promise.all(uniquePaths.map(async (path) => {
+        const keys = (await Promise.all(path.map(async (path) => {
             const listObjectsCommand = new ListObjectsV2Command({
                 Bucket: bucket,
                 Prefix: [prefix, path].filter(Boolean).join("/"),
@@ -26,23 +24,33 @@ async function main() {
             return (response.Contents ?? []).map((obj) => obj.Key ?? "").filter(Boolean);
         }))).flat();
 
-        const uniqueKeys = Array.from(new Set(keys));
+        // Filter out directories that are common prefixes.
+        const uniqueKeys = Array.from(new Set(keys))
+            .filter((a, _, arr) => !arr.some((b) => a !== b && b.startsWith(a)));
 
         await Promise.all(uniqueKeys.map(async (key) => {
+            const filename = join(process.cwd(), prefix.length ? key.slice(key.indexOf("/") + 1) : key);
+            const isDir = filename.at(-1) === "/";
+
+            if (isDir) {
+                await mkdir(filename, { recursive: true });
+                console.info("Created directory:", filename);
+                return;
+            }
+
             const getObjectCommand = new GetObjectCommand({
                 Bucket: bucket,
                 Key: key,
             });
-
             const response = await s3.send(getObjectCommand);
 
             if (response.Body !== undefined) {
-                const filename = join(process.cwd(), prefix.length ? key.slice(key.indexOf("/") + 1) : key);
-                const data = await response.Body.transformToByteArray();
                 await mkdir(dirname(filename), { recursive: true });
-                await writeFile(filename, data);
+                const writeStream = createWriteStream(filename);
+                (response.Body as Readable).pipe(writeStream);
                 console.info("Downloaded:", filename);
             }
+
         }))
     } catch (err) {
         if (err instanceof Error) setFailed(err);
